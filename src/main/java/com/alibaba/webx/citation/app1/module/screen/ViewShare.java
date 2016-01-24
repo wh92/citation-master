e1cfef7b7ca0f331778e8da54eb472fcea8e2bf4
wh92 <wh92_neu@163.com>
2016-01-24 14:22:00 +0800

    201601241421
    viewshare文件更新

--- src/main/java/com/alibaba/webx/citation/app1/module/screen/ViewShare.java
+++ src/main/java/com/alibaba/webx/citation/app1/module/screen/ViewShare.java
@@ -4,14 +4,16 @@
  */
 package com.alibaba.webx.citation.app1.module.screen;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.alibaba.citrus.turbine.Context;
 
@@ -23,7 +25,9 @@ import com.alibaba.citrus.turbine.Context;
 public class ViewShare {
 
     /***/
-    private static final String BASH_BEDROOM_DUANXUEYE_TAS_CON_NASC_RUN_SH = "bash /usr/local/tomcat/citation/run.sh";
+    private static final String AUTHOR_LIST    = "bash /usr/local/tomcat/citation/run.sh";
+    /***/
+    private static final String PREDICT_RUN_SH = "bash /usr/local/tomcat/citation/predict.sh";
 
     /**
      * 
@@ -38,42 +42,35 @@ public class ViewShare {
             String titleId = request.getParameter("titleId");
             String citationCount = "1";
             String authorCount = "1";
-            List<List<String>> shareList = new ArrayList();
-            List<String> rawList=null;
-            List<String> nameList=new ArrayList<String>();
-            List<String> totalList=new ArrayList<String>();
+            List<List<String>> shareList = new ArrayList<List<String>>();
+            List<String> rawList = null;
+            List<String> nameList = new ArrayList<String>();
+            List<String> totalList = new ArrayList<String>();
             //
-            List<String> outputList = getShOutStr(titleId);
+            List<String> outputList = getShOutStr(titleId, AUTHOR_LIST);
             String outputResult = "";
-            int authorC=0;
+            int authorC = 0;
             //StringList Data List
-            for(String line : outputList){           	
-            	
-                if(line.startsWith("Citations")){
-                    citationCount=line.split("\t")[1];
-                }else if(line.startsWith("Author")){
-                    authorCount=line.split("\t")[1];
-                    authorC=Integer.parseInt(authorCount);
-                }else if(line.startsWith("Name")){
-                	//line.replace("Name\t", "");
-                	String[] nameLineList=line.split("\t");
-                	for(int i=0;i<authorC;i++){
-                		nameList.add(nameLineList[i+1]);
-                	}
-                }else if(line.startsWith("total")){
-                	//line.replace("total\t","");
-                	outputResult = outputResult + line;
-                	String[] totalLineList=line.split("\t");
-                	for(int j=0;j<authorC;j++){
-                		totalList.add(totalLineList[j+1]);
-                	}
-                }else{
-                	rawList=new ArrayList();
-                    for(String item : line.split("\t")){
-                    	rawList.add(item);
+            for (String line : outputList) {
+
+                if (line.startsWith("Citations")) {
+                    citationCount = line.split("\t")[1];
+                } else if (line.startsWith("Author")) {
+                    authorCount = line.split("\t")[1];
+                    authorC = Integer.parseInt(authorCount);
+                } else if (line.startsWith("Name")) {
+                    //line.replace("Name\t", "");
+                    String[] nameLineList = line.split("\t");
+                    for (int i = 0; i < authorC; i++) {
+                        nameList.add(nameLineList[i + 1]);
+                    }
+                } else if (line.startsWith("#")) {
+                    rawList = new ArrayList<String>();
+                    for (String item : line.split("\t")) {
+                        rawList.add(item);
                     }
                     shareList.add(rawList);
-                }
+                } 
             }
             context.put("outputResult", outputResult);
             context.put("title", titleStr);
@@ -82,36 +79,40 @@ public class ViewShare {
             context.put("nameList", nameList);
             context.put("shareList", shareList);
             context.put("totalList", totalList);
-            dataMsg(context,outputList);
+            dataMsg(context, outputList);
+            //¡ÌÕ‚“ª’≈Õº
+            predictList(context,titleId);
         } catch (Exception e) {
             context.put("message", e.getMessage());
         }
     }
-    
-    private void dataMsg(Context context, List<String> strList) throws NumberFormatException {
-        int authorCnt = 0;
-        List<String> nameList = new ArrayList<String>();
+
+    /**
+     * 
+     * @param context
+     * @param strList
+     * @throws Exception
+     */
+    private void dataMsg(Context context, List<String> strList) throws Exception {
         List<JSONArray> yearDataList = new ArrayList<JSONArray>();
-        List<JSONObject> nameJSList=new ArrayList<JSONObject>();
+        List<JSONObject> nameJSList = new ArrayList<JSONObject>();
         for (String str : strList) {
             if (str.startsWith("Citations")) {
                 continue;
             } else if (str.startsWith("Author")) {
-                String[] countStr = str.split("\t");
-                //authorCnt = Integer.parseInt(countStr);
                 continue;
             } else if (str.startsWith("Name")) {
-                JSONObject jso=null;
-                String[] nameLineList=str.split("\t");
-            	for(int i=1;i<nameLineList.length;i++){
-            		jso=new JSONObject();
-            		jso.put("name",nameLineList[i]);
-            		nameJSList.add(jso);
-            	}
-            	continue;
-            } else if(str.startsWith("total")){
-            	continue;
-            } else{
+                JSONObject jso = null;
+                String[] nameLineList = str.split("\t");
+                for (int i = 1; i < nameLineList.length; i++) {
+                    jso = new JSONObject();
+                    jso.put("name", nameLineList[i]);
+                    nameJSList.add(jso);
+                }
+                continue;
+            } else if (str.startsWith("#")) {
+                continue;
+            } else {
                 //yearDataList.add(str);
                 String[] yearData = str.split("\t");
 
@@ -128,21 +129,21 @@ public class ViewShare {
         JSONArray dataList = new JSONArray(yearDataList);
         context.put("dataList", dataList);
         context.put("nameJSList", nameJSList.toString());
-        
-    }
 
+    }
 
     /**
      * 
      * @param titleId
+     * @param shell
      * @return
      * @throws Exception 
      */
-    private List<String> getShOutStr(String titleId) throws Exception {
+    private List<String> getShOutStr(String titleId, String shell) throws Exception {
         List<String> outputList = new ArrayList<String>();
         try {
             Process process = null;
-            process = Runtime.getRuntime().exec(BASH_BEDROOM_DUANXUEYE_TAS_CON_NASC_RUN_SH + " " + titleId);
+            process = Runtime.getRuntime().exec(shell + " " + titleId);
             BufferedReader input = new BufferedReader(new InputStreamReader(
                 process.getInputStream()));
 
@@ -158,5 +159,30 @@ public class ViewShare {
         }
         return outputList;
     }
+    /**
+     * 
+     * @param context
+     * @param titleId
+     * @throws Exception
+     * @throws JSONException
+     */
+    private void predictList(Context context, String titleId) throws Exception, JSONException {
+        List<String> outputList = getShOutStr(titleId, PREDICT_RUN_SH);
+        List<JSONObject> predictResultList = new ArrayList<JSONObject>();
+        for (String line : outputList) {
+            if (line.startsWith("This")) {
+                context.put("predictMsg", "No predict result");
+                return;
+            } else {
+                JSONObject citationCount = new JSONObject();
+                citationCount.put("count", line.split("\t")[1]);
+                predictResultList.add(citationCount);
+            }
+        }
+        context.put("predictResult", new JSONArray(predictResultList).toString());
+    }
+
+
+    
 
 }
